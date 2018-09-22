ad0f64a611631c60d731df2439bfc1749079bf2e
DanielJ <danielwjameson@yahoo.com>
2018-09-22 17:20:57 -0500

    Add toolbar

--- app/src/main/java/dnlj/umkc/cs449/tasky/MainActivity.java
+++ app/src/main/java/dnlj/umkc/cs449/tasky/MainActivity.java
@@ -4,19 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 	private RecyclerView recyclerView;
 	private RecyclerView.LayoutManager layoutManager;
 	private RecyclerView.Adapter adapter;
 	
+	private Toolbar toolbar;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		// Get UI elements
-		recyclerView = findViewById(R.id.task_view);
+		// Setup toolbar
+		toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		toolbar.inflateMenu(R.menu.toolbar);
 		
 		// Setup recycler view
 		String[] data = new String[100];
@@ -25,10 +32,18 @@ public class MainActivity extends AppCompatActivity {
 			data[i] = "Element: " + i;
 		}
 		
+		recyclerView = findViewById(R.id.task_view);
 		layoutManager = new LinearLayoutManager(this);
 		adapter = new TaskAdapter(data);
 		recyclerView.setHasFixedSize(true);
 		recyclerView.setLayoutManager(layoutManager);
 		recyclerView.setAdapter(adapter);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.toolbar, menu);
+		return true;
 	}
 }
